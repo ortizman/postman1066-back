576fb739841c0814e623df6e243077aafc8b95ca
Manuel Ortiz <ortizman@gmail.com>
2021-12-09 23:01:54 -0300

    add url domain validation

--- index.mjs
+++ index.mjs
@@ -2,6 +2,8 @@ import fetch from 'node-fetch';
 import express from 'express';
 import cors from "cors";
 
+const port = process.env.PORT || 3000
+
 const app = express()
 
 const corsOptions = {
@@ -11,22 +13,29 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.get('/', async (req, res) => {  
+app.get('/', async (req, res) => {
     res.send('OK');
 })
 
 app.get('/collection', async (req, res) => {
+    const url = "" + req.query.url;
     try {
-        console.log('Fetch: ', req.query.collectionUrl);
-        const collection = await fetch(req.query.collectionUrl)
+        console.log('Fetch: ', url);
+        if(!url.startsWith('https://www.getpostman.com/')) {
+            throw Error('Postman not response 200')
+        }
+        const collection = await fetch(url)
             .then(res => res.json())
+        if (collection.statusCode) {
+            throw Error('Postman not response 200')
+        }
         res.send({ collection });
     } catch (error) {
-        console.error('Error fetching: ', req.query.collectionUrl, error);
+        console.error('Error fetching: ', url, error);
         res.status(400).send('Fetching Error');
     }
 })
 
-app.listen(3000, () => {
-    console.info(`App Ready`);
+app.listen(port, () => {
+    console.info(`App Ready. PORT: ${port}`);
 })

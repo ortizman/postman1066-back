d11a1b1f070ad3df66d17e5a2259036552248fd8
Manuel Ortiz <ortizman@gmail.com>
2021-12-09 21:35:51 -0300

    first commit

---
+++ index.mjs
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch';
+import express from 'express';
+import cors from "cors";
+
+const app = express()
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+app.get('/', async (req, res) => {
+    try {
+        console.log('Fetch: ', req.query.collectionUrl);
+        const collection = await fetch(req.query.collectionUrl)
+            .then(res => res.json())
+        res.send({ collection });
+    } catch (error) {
+        console.error('Error fetching: ', req.query.collectionUrl, error);
+        res.status(400).send('Fetching Error');
+    }
+})
+
+app.listen(3001, () => {
+    console.info(`App Ready`);
+})
